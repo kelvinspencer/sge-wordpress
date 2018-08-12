b12c2f103e46de2519120b18406e1b196d324949
kelvinspencer <kelvin.spencer@gmail.com>
2018-08-11 20:58:17 -0500

    Initial Themes

---
+++ themes/myhome/templates/sidebar-footer.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! is_active_sidebar( 'mh-sidebar-footer' ) ) {
+	return;
+}
+
+dynamic_sidebar( 'mh-sidebar-footer' );
+dynamic_sidebar( 'mh-sidebar-footer2' );
+dynamic_sidebar( 'mh-sidebar-footer3' );
+
+
